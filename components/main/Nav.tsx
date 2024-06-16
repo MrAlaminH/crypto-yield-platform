ec8d34782d31542b4ffad14229848e427f66e7e7
Its-Alamin-H <alaminhosssain97@gmail.com>
2024-06-16 14:11:38 +0600

    updated

--- components/main/Nav.tsx
+++ components/main/Nav.tsx
@@ -15,7 +15,7 @@ const Nav = () => {
   };
 
   return (
-    <nav className="bg-gray-900   dark:bg-gray-900  fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-900">
+    <nav className="bg-[#03001417] backdrop-blur-md fixed w-full z-20 top-0 start-0 ">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-2">
         <div className="flex items-center space-x-3">
           <a href="#home">
@@ -61,7 +61,7 @@ const Nav = () => {
           } w-full md:flex md:items-center md:w-auto`}
           id="navbar-sticky"
         >
-          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-900 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transprent dark:bg-transparent  md:dark:bg-transparent  dark:border-gray-700">
             <li>
               <a
                 href="#Home"
