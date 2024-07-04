f0272e701bc76e6d177cabdc453cb8e39332285f
Its-Alamin-H <alaminhosssain97@gmail.com>
2024-07-04 19:51:20 +0600

    ColorFixed

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -17,7 +17,7 @@ const Navbar = () => {
   return (
     <>
       {/* Navbar for mobile and tablet view */}
-      <nav className="fixed w-full z-20 top-0 border-b border-gray-200 dark:border-gray-600 bg-gray-900 md:hidden">
+      <nav className="fixed w-full z-20 top-0 border-b border-gray-200 bg-gray-900 md:hidden">
         <div className="max-w-screen-xl flex items-center justify-between mx-auto p-4">
           <Link
             href="https://crypto-yield-platform.vercel.app/"
@@ -30,14 +30,14 @@ const Navbar = () => {
               height={50}
               className="cursor-pointer hover:animate-slowspin"
             />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
               Soltitude
             </span>
           </Link>
           <div className="flex">
             <button
               type="button"
-              className="font-semibold bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/40 rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 mt-2"
+              className="font-semibold bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:outline-none focus:ring-blue-300 shadow-lg shadow-blue-500/50 rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 mt-2"
             >
               Wallet Connect
             </button>
@@ -46,7 +46,7 @@ const Navbar = () => {
       </nav>
 
       {/* Existing navbar for desktop view (hidden on mobile and tablet) */}
-      <nav className="hidden md:flex fixed w-full z-20 md:top-0 md:border-b border-gray-200 dark:border-gray-600 bg-gray-900">
+      <nav className="hidden md:flex fixed w-full z-20 md:top-0 md:border-b border-gray-200 bg-gray-900">
         <div className="max-w-screen-xl flex items-center justify-between mx-auto p-4 space-x-40">
           <div className="flex items-center space-x-3">
             <Link href="https://crypto-yield-platform.vercel.app/">
@@ -67,7 +67,7 @@ const Navbar = () => {
               <li>
                 <Link
                   href="/stake"
-                  className="flex items-center gap-2 py-2 px-3 text-blue-700 md:text-blue-700 md:p-0 md:dark:text-blue-500"
+                  className="flex items-center gap-2 py-2 px-3 text-blue-700 md:text-blue-700 md:p-0"
                   aria-current="page"
                 >
                   <MdOutlineRocketLaunch /> Stake
@@ -76,7 +76,7 @@ const Navbar = () => {
               <li>
                 <Link
                   href="/unstake"
-                  className="flex items-center gap-2 py-2 px-3 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  className="flex items-center gap-2 py-2 px-3 text-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0"
                 >
                   <BsStars /> Unstake
                 </Link>
@@ -84,7 +84,7 @@ const Navbar = () => {
               <li>
                 <Link
                   href="/defi"
-                  className="flex items-center gap-2 py-2 px-3 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  className="flex items-center gap-2 py-2 px-3 text-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0"
                 >
                   <HiOutlineTrophy /> Benefits
                 </Link>
@@ -92,7 +92,7 @@ const Navbar = () => {
               <li>
                 <Link
                   href="/rewards"
-                  className="flex items-center gap-2 py-2 px-3 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  className="flex items-center gap-2 py-2 px-3 text-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0"
                 >
                   <HiOutlineBolt /> Rewards
                 </Link>
@@ -102,7 +102,7 @@ const Navbar = () => {
           <div>
             <button
               type="button"
-              className="font-semibold bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/40 rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 mt-2"
+              className="font-semibold bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:outline-none focus:ring-blue-300 shadow-lg shadow-blue-500/50 rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 mt-2"
             >
               Wallet Connect
             </button>
@@ -125,7 +125,7 @@ const Navbar = () => {
           <li>
             <Link
               href="/unstake"
-              className="flex flex-col items-center text-gray-900 dark:text-white"
+              className="flex flex-col items-center text-white"
             >
               <BsStars size={24} />
               <span className="text-sm">Unstake</span>
@@ -134,7 +134,7 @@ const Navbar = () => {
           <li>
             <Link
               href="/defi"
-              className="flex flex-col items-center text-gray-900 dark:text-white"
+              className="flex flex-col items-center  text-white"
             >
               <HiOutlineTrophy size={24} />
               <span className="text-sm">Benefits</span>
@@ -143,7 +143,7 @@ const Navbar = () => {
           <li>
             <Link
               href="/rewards"
-              className="flex flex-col items-center text-gray-900 dark:text-white"
+              className="flex flex-col items-center  text-white"
             >
               <HiOutlineBolt size={24} />
               <span className="text-sm">Rewards</span>
